dbfa14428f5b4685e401fffa6dc2aa7f7290a1a8
Charles Lee <pcrlee85@gmail.com>
2018-04-17 10:53:56 -0600

    3rd Commit

--- lib/company.rb
+++ lib/company.rb
@@ -1,7 +1,13 @@
 class Company
-  attr_reader :name
+  attr_reader :name,
+              :employees,
+              :projects,
+              :timesheets
 
   def initialize
     @name = name
+    @employees = []
+    @projects = []
+    @timesheets = []
   end
 end
--- test/company_test.rb
+++ test/company_test.rb
@@ -4,7 +4,25 @@ require './lib/company'
 class CompanyTest < Minitest::Test
   def test_company_exists
     company = Company.new
-    
-    assert_equal company, company.name
+
+    assert company, company.name
+  end
+
+  def test_company_has_employees
+    company = Company.new
+
+    assert_equal [], company.employees
+  end
+
+  def test_company_has_projects
+    company = Company.new
+
+    assert_equal [], company.projects
+  end
+
+  def test_company_has_timesheets
+    company = Company.new
+
+    assert_equal [], company.timesheets
   end
 end
