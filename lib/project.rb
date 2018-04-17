e1e920fd52defceca103a1bb65faa22d816a9d03
Charles Lee <pcrlee85@gmail.com>
2018-04-17 11:18:01 -0600

    5th commit

--- lib/project.rb
+++ lib/project.rb
@@ -1,7 +1,15 @@
 class Project
-  attr_reader :exists
+  attr_reader :exists,
+              :id,
+              :name,
+              :start_date,
+              :end_date
 
   def initialize(project_id, name, start_date, end_date)
     @exists = exists
+    @id = 123
+    @name = name
+    @start_date = start_date
+    @end_date = end_date
   end
 end
--- test/project_test.rb
+++ test/project_test.rb
@@ -7,4 +7,13 @@ class ProjectTest < Minitest::Test
 
     assert project, project.exists
   end
+
+  def test_project_has_attributes
+    project = Project.new('123', 'Widget Maker', '2015-01-01', '2018-01-01')
+
+    assert_equal Integer, project.id.class
+    assert_equal "Widget Maker", project.name
+    assert_equal "2015-01-01", project.start_date
+    assert_equal "2018-01-01", project.end_date
+  end
 end
